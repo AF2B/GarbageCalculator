f61c40bb65163f0af747ebc4af212bd885c8d5d2
andre.borba <dev_andre.filipe@outlook.com>
2023-10-12 18:09:54 -0300

    BATMAN

--- GarbageCalculator/ViewController.swift
+++ GarbageCalculator/ViewController.swift
@@ -8,12 +8,66 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var firstNumber: UITextField!
+    @IBOutlet weak var secondNumber: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+
+    var inputFirstNumber: Int = 0
+    var inputSecondNumber: Int = 0
+    var resultOutput: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    @IBAction func clearFields(_ sender: UIButton) {
+        firstNumber.text = ""
+        secondNumber.text = ""
+        resultLabel.text = "Result: 0"
+    }
 
+    @IBAction func sum(_ sender: UIButton) {
+        if let firstNumberText = firstNumber.text, let secondNumberText = secondNumber.text,
+           let firstNumber = Int(firstNumberText), let secondNumber = Int(secondNumberText) {
+            resultOutput = firstNumber + secondNumber
+            resultLabel.text = String(resultOutput)
+        } else {
+            resultLabel.text = "Invalid input"
+        }
+    }
+
+    @IBAction func subtract(_ sender: UIButton) {
+        if let firstNumberText = firstNumber.text, let secondNumberText = secondNumber.text,
+           let firstNumber = Int(firstNumberText), let secondNumber = Int(secondNumberText) {
+            resultOutput = firstNumber - secondNumber
+            resultLabel.text = String(resultOutput)
+        } else {
+            resultLabel.text = "Invalid input"
+        }
+    }
+
+    @IBAction func multiply(_ sender: UIButton) {
+        if let firstNumberText = firstNumber.text, let secondNumberText = secondNumber.text,
+           let firstNumber = Int(firstNumberText), let secondNumber = Int(secondNumberText) {
+            resultOutput = firstNumber * secondNumber
+            resultLabel.text = String(resultOutput)
+        } else {
+            resultLabel.text = "Invalid input"
+        }
+    }
+
+    @IBAction func divide(_ sender: UIButton) {
+        if let firstNumberText = firstNumber.text, let secondNumberText = secondNumber.text,
+           let firstNumber = Int(firstNumberText), let secondNumber = Int(secondNumberText) {
+            if secondNumber != 0 {
+                resultOutput = firstNumber / secondNumber
+                resultLabel.text = String(resultOutput)
+            } else {
+                resultLabel.text = "Division by zero"
+            }
+        } else {
+            resultLabel.text = "Invalid input"
+        }
+    }
 }
 
